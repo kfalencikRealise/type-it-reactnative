ddf090c8ddabf7f106628d9f55a0802f786795db
kfalencik <kfalencik@gmail.com>
2019-12-10 17:14:08 +0000

    Game start screen

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -1,27 +1,37 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView, Dimensions } from 'react-native';
-
-import Background from '../components/Background';
+import { View, Text, StyleSheet, Button, ImageBackground, Image, ScrollView, Dimensions } from 'react-native';
 
 const GameStart = props => {
   return (
-    <Background>
-      <View style={styles.screen}>
-        <Text>Please select difficulty level</Text>
+    <View style={styles.screen}>
+      <ImageBackground style={{width: '100%', height: '100%', resizeMode: 'cover', backgroundColor: 'aqua', flex: 1, justifyContent: 'center'}} source={require('../assets/background-screen1.png')}>
 
-        <View>
-          <Button title="Easy" onPress={() => {
-            props.navigation.replace('Game');
-          }} />
-          <Button title="Normal" onPress={() => {
-            props.navigation.replace('Game');
-          }} />
-          <Button title="Hard" onPress={() => {
-            props.navigation.replace('Game');
-          }} />
+        <View style={styles.box}>          
+          <ImageBackground style={styles.boxBackground} source={require('../assets/board.png')}>
+            <Image style={styles.logo} source={require('../assets/logo.png')} />
+            <Text>The objective of the game is to type in the word shown as quickly as possible but look out as the time is ticking and every misspelled word results in a time penalty.</Text>
+
+            <View style={styles.button}>
+              <Button title="Easy" onPress={() => {
+                props.navigation.replace('Game');
+              }} />
+            </View>
+            
+            <View style={styles.button}>
+              <Button title="Normal" onPress={() => {
+                props.navigation.replace('Game');
+              }} />
+            </View>
+
+            <View style={styles.button}>
+              <Button title="Hard" onPress={() => {
+                props.navigation.replace('Game');
+              }} />
+            </View>
+          </ImageBackground>
         </View>
-      </View>
-    </Background>
+      </ImageBackground>
+    </View>
   );
 };
 
@@ -32,5 +42,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 15
+  },  
+  box: {
+    marginHorizontal: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 335
+  },
+  boxBackground: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 335
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 5
   }
 });
